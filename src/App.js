859b2740daa473a518bf12f4a5d080a69ad3aa29
Deian Petrov <deian.petrov@sap.com>
2020-05-03 12:18:26 +0300

    Add local proxy against cors Get all active todos from backend, store in state.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [activeTasks, setActiveTasks] = useState([]);
+
+  useEffect(() => {
+    console.log("Effect has been run");
+    getActiveTasks();
+  }, []);
+
+  const getActiveTasks = async () => {
+    //add await every time we have a promise
+    const response = await fetch(
+      `/todos/active`
+    );
+    const data = await response.json();
+    console.log(data);
+    setActiveTasks(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weekly Todos</h1>
     </div>
   );
 }
